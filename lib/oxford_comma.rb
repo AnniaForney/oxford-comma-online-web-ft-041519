57dbdd3ea97ddab729c4f06ac4c559f60b1e4fa0
Annia Forney <anniaforney@gmail.com>
2019-04-20 04:13:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,18 @@
 def oxford_comma(array)
-
+return array.first 
 end
 
-def oxford_comma
+def oxford_comma(array)
+  if array.length === 2 
+  else 
 end 
 
-def oxford_comma
+def oxford_comma(array)
+  if array.length === 3 
+    
 end 
 
 def oxford_comma(array)
   if array.length > 3
-  else 
+  else "less than is not an array"
 end 
